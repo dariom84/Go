d88e6045f1bd23a5789d4a409650ff75c9e77653
Abel <abelbarahona@gmail.com>
2016-10-20 20:52:39 -0300

    boton start

--- app/src/main/java/com/example/om84/go/activity/DetalleActivity.java
+++ app/src/main/java/com/example/om84/go/activity/DetalleActivity.java
@@ -1,12 +1,23 @@
 package com.example.om84.go.activity;
 
+import android.icu.util.Calendar;
+import android.net.sip.SipAudioCall;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
+
 
 import com.example.om84.go.R;
 
 public class DetalleActivity extends AppCompatActivity {
+    Chronometer focus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,18 @@ public class DetalleActivity extends AppCompatActivity {
 
         tvNombreRutina.setText(parametros.getString("nombreRutina"));
         tvDetalleRutina.setText(parametros.getString("detalleRutina"));
+
+        final Button start = (Button) findViewById(R.id.startRutina);
+        focus = new Chronometer (DetalleActivity.this);
+        final TextView chronometer = (TextView) findViewById(R.id.displayChronometer);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                focus.start();
+                chronometer.setText(focus.getText());
+
+
+            }
+        });
     }
+
 }
